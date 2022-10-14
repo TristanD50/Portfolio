fa5c4484dfec0315e7db134538f1df2bd8330320
Tristan DAVID <tristandavid50@yahoo.fr>
2022-10-14 10:57:30 +0200

    start

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from './router'
+import store from './store'
 
 new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
